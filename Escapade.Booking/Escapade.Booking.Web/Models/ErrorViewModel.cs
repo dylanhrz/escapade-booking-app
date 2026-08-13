68145b056723273e94ec43ba9a8cb0a888d0e9ad
dylanhrz <dylan.hourez@student.howest.be>
2026-08-13 21:43:04 +0200

    Initialise project with EF Core and appsettings

---
+++ Escapade.Booking/Escapade.Booking.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Escapade.Booking.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
