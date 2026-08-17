6c9164447f44c20aa0da519944f9e4a0cb5354c1
dylanhrz <dylan.hourez@student.howest.be>
2026-08-17 11:55:48 +0200

    Add booking post method and redirect view

--- Escapade.Booking/Escapade.Booking.Web/Controllers/BookingController.cs
+++ Escapade.Booking/Escapade.Booking.Web/Controllers/BookingController.cs
@@ -1,10 +1,20 @@
+using System.Diagnostics;
+using Escapade.Booking.Web.Data;
+using Escapade.Booking.Web.Models;
 using Escapade.Booking.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Escapade.Booking.Web.Controllers;
 
 public class BookingController : Controller
 {
+    private readonly EscapadeDbContext _escapadeDbContext;
+
+    public BookingController(EscapadeDbContext escapadeDbContext)
+    {
+        _escapadeDbContext = escapadeDbContext;
+    }
     
     public IActionResult Index()
     {
@@ -24,12 +34,40 @@ public class BookingController : Controller
     [ValidateAntiForgeryToken]
     public IActionResult NewBooking(BookingNewBookingViewModel bookingNewBookingViewModel)
     {
+        if (!ModelState.IsValid)
+        {
+            return View(bookingNewBookingViewModel);
+        }
+        
         if (bookingNewBookingViewModel.EndDate <= bookingNewBookingViewModel.StartDate)
         {
             ModelState.AddModelError("EndDate", "Vertrekdatum moet na de aankomstdatum liggen.");
         }
 
-        return View();
+        var booking = new Core.Entities.Booking()
+        {
+            Name = bookingNewBookingViewModel.Name,
+            Email = bookingNewBookingViewModel.Email,
+            PhoneNumber = bookingNewBookingViewModel.PhoneNumber,
+            NumberOfGuests = bookingNewBookingViewModel.NumberOfGuests,
+            Comment = bookingNewBookingViewModel.Comment,
+            StartDate = bookingNewBookingViewModel.StartDate,
+            EndDate = bookingNewBookingViewModel.EndDate
+        };
+        
+        _escapadeDbContext.Bookings.Add(booking);
+        
+        try
+        {
+            _escapadeDbContext.SaveChanges();
+        }
+        catch (DbUpdateException dbUpdateException)
+        {
+            Debug.WriteLine(dbUpdateException.Message);
+            return View("Error", new ErrorViewModel());
+        }
+
+        return RedirectToAction("Index");
     }
 
     
