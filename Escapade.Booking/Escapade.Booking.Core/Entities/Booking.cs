a20919ce90e18b63efcf976002ef944909ce06ea
dylanhrz <dylan.hourez@student.howest.be>
2026-08-26 16:45:53 +0200

    Make comments for booking not requierd

--- Escapade.Booking/Escapade.Booking.Core/Entities/Booking.cs
+++ Escapade.Booking/Escapade.Booking.Core/Entities/Booking.cs
@@ -6,10 +6,8 @@ public class Booking
     public string Name { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
-    
     public int NumberOfGuests { get; set; }
-    public string Comment { get; set; }
-    
+    public string? Comment { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
 
