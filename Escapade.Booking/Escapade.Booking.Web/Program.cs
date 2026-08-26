79dcf3e8ba3db6e4d7cb70bf7c7f09ee21079683
dylanhrz <dylan.hourez@student.howest.be>
2026-08-26 18:43:44 +0200

    Add dynamic logout button to admin

--- Escapade.Booking/Escapade.Booking.Web/Program.cs
+++ Escapade.Booking/Escapade.Booking.Web/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<EscapadeDbContext>(options =>
@@ -20,11 +19,9 @@ builder.Services.AddSession(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -33,6 +30,7 @@ app.UseHttpsRedirection();
 app.UseSession();
 app.UseStaticFiles();
 app.UseRouting();
+
 app.UseAuthorization();
 
 app.MapStaticAssets();
