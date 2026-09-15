6d2816c225d939a07a835d8b7b842ac00c90d678
dylanhrz <dylan.hourez@student.howest.be>
2026-09-15 13:59:25 +0200

    Add about controller & actions

---
+++ Escapade.Booking/Escapade.Booking.Web/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Escapade.Booking.Web.Controllers;
+
+public class AboutController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult Rooms()
+    {
+        return View();
+    }
+    
+    public IActionResult Rocquingy()
+    {
+        return View();
+    }
+}
