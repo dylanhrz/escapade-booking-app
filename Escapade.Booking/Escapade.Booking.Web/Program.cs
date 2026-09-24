857722809cf7343043c60d02f451ffa3111d2d40
dylanhrz <dylan.hourez@student.howest.be>
2026-09-24 14:18:03 +0200

    Remove admin link in footer and create custom route

--- Escapade.Booking/Escapade.Booking.Web/Program.cs
+++ Escapade.Booking/Escapade.Booking.Web/Program.cs
@@ -51,6 +51,11 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
+    name: "admin",
+    pattern: "admin/{action=Login}/{id?}",
+    defaults: new { controller = "Admin" });
+
+app.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
